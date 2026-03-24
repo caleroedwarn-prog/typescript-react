6a9bf8f27071ebc881e74fff86e0750a9556b581
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-24 03:42:08 -0500

    creating a homepage for the Todo App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import Number from "./useStateWithNumber";
-import String from './useStateWithString';
 
 function App() {
   return (
     <div className="App">
-      <Number />
-      <br />
-      <br/>
-      <String />
     </div>
   );
 }
