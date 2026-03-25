2ceb5e9f32ee16b8f7db3f307508971f597dc2f1
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-25 17:24:48 +0100

    creating and rendering tasks

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+import TaskForm from "./form";
+
 function Tasks(){
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return(
-        <>
+        <div className={`task-page ${isModalOpen ? "blurred" : ""}`}>
         <h2> Tasks </h2>
-        </>
+        <button type="button" className="add-task-btn" onClick={() => setIsModalOpen(true)}> + </button>
+
+        {/* modal */}
+        {isModalOpen && (
+            <div className="modal-overlay" onClick={() => setIsModalOpen(false)}>
+                <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                    <button type="button" title="Close" className="Modal-close" onClick={() => setIsModalOpen(false)}>&times;</button>
+                    <form />
+                </div>
+            </div>
+        )}
+        <div className="task-list">
+            
+        </div>
+        </div>
     );
 }
 export default Tasks;
