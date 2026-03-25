00c463f1bc687bec29d4a02b088dc1c5263fc531
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-25 14:29:02 +0100

    Created the logical hambourger toggle open and close

--- src/components/Hambourger.tsx
+++ src/components/Hambourger.tsx
@@ -1,7 +1,13 @@
 import "../static/hambourger.css";
+import { useState } from "react";
 function Hambourger(){
+    const[isToggle, setIsToggle] = useState(false);
+
+    const toggleState = () =>{
+        setIsToggle(!isToggle);
+    }
     return(
-        <div className="Hambourger">    
+        <div className={isToggle ?  "Hambourger active" : "Hambourger"} onClick={toggleState}>    
             <span></span>
             <span></span>
             <span></span>
