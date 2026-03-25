fa132d7c826acd22246448d102bc234d55ad8f6e
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-25 04:56:08 +0100

    imported browserRouter, routes and route from react dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Homepage from './pages/homePage';
 import './static/homePage.css'
+import Tasks from './pages/Task';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Homepage />
+
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/tasks' element={<Tasks />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
-
+import { Link } from "react-router-dom";
 function Navbar(){
 return(
-    <></>
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/tasks">Task</Link>
+    </nav>
 )
 }
 export default Navbar;
