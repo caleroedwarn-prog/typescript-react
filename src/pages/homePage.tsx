b1912389ca1048fe5926e72ffabde1af8d564041
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-25 03:05:59 +0100

    updating the style

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -2,9 +2,9 @@ import '../static/homePage.css';
 function Homepage() {
 
     return(
-        <>
+        <div className='mainHeader'>
         <h1>ToDO Prior</h1>
-        </>
+        </div>
     )
 
 }
