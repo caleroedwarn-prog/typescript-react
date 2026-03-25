b5b81ac0cf24c70fce87e18c635aaf5c365bfe47
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-25 14:04:10 +0100

    Created a hambourger that contains other pages

---
+++ src/components/Hambourger.tsx
@@ -0,0 +1,13 @@
+function Hambourger(){
+    return(
+        <div className="Hambourger">
+            <div className="open">
+                <div className="close">&times;</div>
+            <span></span>
+            <span></span>
+            <span></span>
+            </div>
+        </div>
+    );
+}
+export default Hambourger;
