a7eee6a5bb52dbbc98dbb1561667023b15d2bcb6
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-04-03 03:59:21 +0100

    creating a previouse task container where each task completed and parmenently deleted will all go into the previouseTask

--- src/pages/previoustask.tsx
+++ src/pages/previoustask.tsx
@@ -1,4 +1,24 @@
+import { useOutletContext } from "react-router-dom";
+import { TaskType } from "../components/Layout";
+
+type contextType = {
+    previouseTask: TaskType[];
+    setPreviouseTask: React.Dispatch<React.SetStateAction<TaskType[]>>;
+
+    deletedTask: TaskType[];
+    setDeletedTask: React.Dispatch<React.SetStateAction<TaskType[]>>;
+
+    task: TaskType[];
+    setTask: React.Dispatch<React.SetStateAction<TaskType[]>>;
+};
 function PreviouseTask(){
+    const { previouseTask, setPreviouseTask, setDeletedTask } = useOutletContext<contextType>();
+
+    const completeTask = (index: number) => {
+        if(!previouseTask || index < 0 || index >= previouseTask.length) return;
+
+        const taskToComplete = previouseTask[index];
+    }
     return(
         <>
         <h2>Previous Tasks</h2>
