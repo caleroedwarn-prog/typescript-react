0ecb313c8d54caf16b674f9c49d30726b60d89de
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-04-08 13:57:09 +0100

    update

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -2,64 +2,13 @@ import "../static/homePage.css";
 import React from "react";
 import "../images/note.png";
 import { Link } from "react-router-dom";
-import noteImg from "../images/note.png";
+import ImageWelcome from "../subpages/home";
 import writingImg from "../images/writing.jpg";
 
 function Homepage() {
   return (
     <>
-      <div className="background">
-        <div className="background-image">
-          <img src={noteImg} alt="message" />
-        </div>
-      </div>
-      <div className="main-content">
-        <pre>
-          <h1
-            style={{
-              fontSize: "30px",
-              position: "relative",
-              left: "30px",
-              top: "90px",
-            }}
-          >
-            Set Your TASKS <br />
-          </h1>
-          <h1
-            style={{
-              position: "relative",
-              left: "50px",
-              fontSize: "70px",
-            }}
-          >
-            with TODO PRIOR
-          </h1>
-
-          <br />
-          <b
-            style={{
-              position: "relative",
-              left: "100px",
-            }}
-          >
-            {" "}
-            start your discipline here{" "}
-            <Link to="/tasks" className="nav-link">
-              {" "}
-              <span
-                style={{
-                  background: "green",
-                  borderRadius: "20px",
-                  width: "fit",
-                  fontSize: "20px",
-                }}
-              >
-                - GET STARTED
-              </span>
-            </Link>
-          </b>
-        </pre>
-      </div>
+    <ImageWelcome />
 
       <h3
         style={{
--- src/subpages/home.tsx
+++ src/subpages/home.tsx
@@ -1,5 +1,62 @@
+import noteImg from "../images/note.png";
+import { Link } from "react-router-dom";
+
 function ImageWelcome(){
     return(
-        
+        <>
+        <div className="background">
+        <div className="background-image">
+          <img src={noteImg} alt="message" />
+        </div>
+      </div>
+      <div className="main-content">
+        <pre>
+          <h1
+            style={{
+              fontSize: "30px",
+              position: "relative",
+              left: "30px",
+              top: "90px",
+            }}
+          >
+            Set Your TASKS <br />
+          </h1>
+          <h1
+            style={{
+              position: "relative",
+              left: "50px",
+              fontSize: "70px",
+            }}
+          >
+            with TODO PRIOR
+          </h1>
+
+          <br />
+          <b
+            style={{
+              position: "relative",
+              left: "100px",
+            }}
+          >
+            {" "}
+            start your discipline here{" "}
+            <Link to="/tasks" className="nav-link">
+              {" "}
+              <span
+                style={{
+                  background: "green",
+                  borderRadius: "20px",
+                  width: "fit",
+                  fontSize: "20px",
+                }}
+              >
+                - GET STARTED
+              </span>
+            </Link>
+          </b>
+        </pre>
+      </div>
+      </>
     )
 }
+export default ImageWelcome;
