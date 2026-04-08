14c886081a4337b0de228de669ca2083c8f6e670
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-04-08 15:08:36 +0100

    footer with contact info, supports and terms and conditions

---
+++ src/subpages/footer.tsx
@@ -0,0 +1,47 @@
+function Footer(){
+    return (
+      <div className="Footer">
+        <span className="Contact">
+          <h2>contact info</h2>
+          <p>
+            Email:{" "}
+            <a href="mailto:buameeemmanuel@gmail.com">
+              buameeemmanuel@gmail.com
+            </a>
+          </p>
+          <p>
+            Phone: <a href="tel:+237688029813">+237 688029813</a>
+          </p>
+          <p>
+            Whatsapp:{" "}
+            <a
+              href="https://wa.me/237688029813"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              +237 688029813
+            </a>
+          </p>
+          <p>
+            github:{" "}
+            <a
+              href="https://github.com/caleroedwarn-prog"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CaleroEdwarn-prog
+            </a>
+          </p>
+          <p>
+            LinkedIn:{" "}
+            <a href="https://linkedin.com/in/buameeemmanuel" target="_blank" rel="noopener noreferrer">
+              Buamee Emmanuel
+            </a>
+          </p>
+        </span>
+        <p className="Contact">Supports</p>
+        <p className="Contact">Terms and Conditions</p>
+      </div>
+    );
+}
+export default Footer;
